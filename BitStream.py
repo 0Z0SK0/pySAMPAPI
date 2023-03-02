3615d65d58dfd975ca41006720d9fe9f68b68164
devblackhunter <45397007+devblackhunter@users.noreply.github.com>
2023-03-02 18:05:35 +0500

    Update BitStream.py
    Add ReadBit, Read, ResetReadPointer, ResetWritePointer, Write0, Write1, AlignWriteToByteBoundary, AlignReadToByteBoundary, WriteBits, SetData

--- BitStream.py
+++ BitStream.py
@@ -61,6 +61,96 @@ class BitStream(Structure):
         self.numberOfBitsUsed = c_int(0)
         self.readOffset = c_int(0)
 
+    def ReadBit(self):
+        if(self.data[self.readOffset >> 3] & (0x80 >> (self.readOffset & 7))):
+            self.readOffset=self.readOffset+c_int(1)
+            return c_bool(True)
+
+        else:
+            self.readOffset=self.readOffset+c_int(1)
+            return c_bool(False)
+
+    def Read(self, numberOfBytes = c_int()):
+        if((self.readOffset & 7) == 0):
+            if(self.readOffset + (numberOfBytes << 3) > self.numberOfBitsUsed):
+                return False
+            
+            self.ret = self.data + (self.readOffset >> 3)
+            self.readOffset = self.readOffset + (numberOfBytes << 3)
+
+            return self.ret
+        else:
+            return self.ReadBits(numberOfBytes*8)
+        
+    def ResetReadPointer(self):
+        self.readOffset = c_int(0)
+
+    def ResetWritePointer(self):
+        self.numberOfBitsUsed = c_int(0)
+
+    def Write0(self):
+        # self.AddBitsAndReallocate(c_int(1))
+        if((self.numberOfBitsUsed & 7) == 0):
+            self.data[self.numberOfBitsUsed >> 3] = c_char_p(0);
+
+        self.numberOfBitsUsed = self.numberOfBitsUsed + c_int(1)
+
+    def Write1(self):
+        # self.AddBitsAndReallocate(c_int(1))
+        self.numberOfBitsMod8 = self.numberOfBitsUsed & 7
+        if(self.numberOfBitsMod8 == 0):
+            self.data[self.numberOfBitsUsed >> 3] = 0x80
+        
+        else:
+            self.data[self.numberOfBitsUsed >> 3] |= 0x80 >> (self.numberOfBitsMod8)
+
+        self.numberOfBitsUsed = self.numberOfBitsUsed + c_int(1)
+
+    def AlignWriteToByteBoundary(self):
+        if(self.numberOfBitsUsed):
+            self.numberOfBitsUsed = self.numberOfBitsUsed + 8 - (((self.numberOfBitsUsed - 1) & 7) + 1)
+
+    def AlignReadToByteBoundary(self):
+        if(self.readOffset):
+            self.readOffset = self.readOffset + 8 - (((self.readOffset - 1) & 7) + 1)
+
+    def WriteBits(self, input = c_char_p(), numberOfBitsToWrite = c_int(), rightAlignedBits = c_bool()):
+        if(numberOfBitsToWrite <= 0):
+            return
+        
+        # self.AddBitsAndReallocate(numberOfBitsToWrite)
+        self.offset = c_int(0)
+        self.dataByte = c_char()
+        self.numberOfBitsUsedMod8 = self.numberOfBitsUsed & 7
+
+        while(numberOfBitsToWrite > 0):
+            self.dataByte = (input + self.offset)
+
+            if(numberOfBitsToWrite < 8 and rightAlignedBits):
+                self.dataByte = self.dataByte << 8 - numberOfBitsToWrite
+            
+            if(self.numberOfBitsUsedMod8 == 0):
+                self.data = self.data + (self.numberOfBitsUsed >> 3) + self.dataByte
+
+            else:
+                self.data = self.data + (self.numberOfBitsUsed >> 3) | self.dataByte >> (self.numberOfBitsUsedMod8)
+
+                if(8 - (self.numberOfBitsUsedMod8) < 8 and 8 - (self.numberOfBitsUsedMod8) < numberOfBitsToWrite):
+                    self.data = self.data + (self.numberOfBitsUsed >> 3) + c_int(1) + self.dataByte << (8 - self.numberOfBitsUsedMod8)
+
+            if(numberOfBitsToWrite >= 8):
+                self.numberOfBitsUsed = self.numberOfBitsUsed + c_int(8)
+            else:
+                self.numberOfBitsUsed = self.numberOfBitsUsed + numberOfBitsToWrite
+
+            numberOfBitsToWrite = numberOfBitsToWrite - c_int(8)
+
+            self.offset = self.offset + c_int(1)
+
+    def SetData(self, input = c_char_p()):
+        self.data = input
+        self.copyData = c_bool(False)
+
     def Write(self, bitStream = classmethod, numberOfBits = c_int(), input = c_char_p(), numberOfBytes = c_int()):
         if(bitStream):
             if(numberOfBits):
@@ -72,21 +162,21 @@ class BitStream(Structure):
                         if(bitStream.data[bitStream.readOffset >> 3] & (0x80 >> (bitStream.readOffset % 8))):
                             # write 1
                             self.data[self.numberOfBitsUsed >> 3] = c_char_p(0x80)
-                            bitStream.readOffset=bitStream.readOffset+1
+                            bitStream.readOffset=bitStream.readOffset + c_int(1)
 
                         else:
                             # write 2
                             self.data[self.numberOfBitsUsed >> 3] = c_char_p(0)
-                            bitStream.readOffset=bitStream.readOffset+1
+                            bitStream.readOffset=bitStream.readOffset + c_int(1)
 
                     else:
                         # existing bytes
                         if(bitStream.data[bitStream.readOffset >> 3] & (0x80 >> (bitStream.readOffset % 8))):
                             self.data[self.numberOfBitsUsed >> 3] |= 0x80 >> (self.numberOfBitsMod8) # Set the bit to 1
-                            bitStream.readOffset=bitStream.readOffset+1
+                            bitStream.readOffset=bitStream.readOffset + c_int(1)
 
                     numberOfBits = numberOfBits-1
-                    self.numberOfBitsUsed = self.numberOfBitsUsed+1
+                    self.numberOfBitsUsed = self.numberOfBitsUsed + c_int(1)
 
             else:
                 self.Write(bitStream, bitStream.GetNumberOfBitsUsed())
@@ -104,3 +194,21 @@ class BitStream(Structure):
             else:
                 # WriteBits( ( unsigned char* ) input, numberOfBytes * 8, true );
                 pass
+
+    def WriteAlignedBytes(self, input = c_char_p(), numberOfBytesToWrite = c_int()):
+        self.AlignWriteToByteBoundary()
+        self.Write(input, numberOfBytesToWrite)
+
+    def ReadAlignedBytes(self, numberOfBytesToRead = c_int()):
+        if(numberOfBytesToRead <= 0):
+            return False
+        
+        self.AlignReadToByteBoundary()
+
+        if(self.readOffset + (numberOfBytesToRead << 3) > self.numberOfBitsUsed):
+            return False
+        
+        self.ret = self.data + (self.readOffset >> 3)
+        self.readOffset = self.readOffset + (numberOfBytesToRead << 3)
+
+        return self.ret
